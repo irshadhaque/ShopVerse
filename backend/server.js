6d10219c421ff2c99b79b6221a5c4161a4a7fafe
irshadhaque <irshadul.haque2508@gmail.com>
2025-08-02 02:45:18 +0530

    fixed some bugs and config for deployment

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import path from "path";
-
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -6,9 +6,9 @@ import FeaturedProducts from "../components/FeaturedProducts";
 const categories = [
 	{ href: "/jeans", name: "Jeans", imageUrl: "/jeans.jpg" },
 	{ href: "/t-shirts", name: "T-shirts", imageUrl: "/tshirts.jpg" },
+	{ href: "/jackets", name: "Jackets", imageUrl: "/jackets.jpg" },
 	{ href: "/shoes", name: "Shoes", imageUrl: "/shoes.jpg" },
 	{ href: "/glasses", name: "Glasses", imageUrl: "/glasses.png" },
-	{ href: "/jackets", name: "Jackets", imageUrl: "/jackets.jpg" },
 	{ href: "/suits", name: "Suits", imageUrl: "/suits.jpg" },
 	{ href: "/bags", name: "Bags", imageUrl: "/bags.jpg" },
 ];
@@ -24,7 +24,7 @@ const HomePage = () => {
 		<div className='relative min-h-screen text-white overflow-hidden'>
 			<div className='relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16'>
 				<h1 className='text-center text-5xl sm:text-6xl font-bold text-emerald-400 mb-4'>
-					Explore Our Categories
+					Right to Fashion SALE
 				</h1>
 				<p className='text-center text-xl text-gray-300 mb-12'>
 					Discover the latest trends in eco-friendly fashion
