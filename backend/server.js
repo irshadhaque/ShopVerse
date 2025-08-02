55bf59ddadf5e26018c88494e089dbcdd81124c8
irshadhaque <irshadul.haque2508@gmail.com>
2025-08-02 13:08:26 +0530

    Fix: Re-added .gitignore and cleaned node_modules from Git tracking

--- backend/server.js
+++ backend/server.js
@@ -8,15 +8,25 @@ import cartRoutes from "./routes/cart.route.js";
 import couponRoutes from "./routes/coupon.route.js";
 import paymentRoutes from "./routes/payment.route.js";
 import analyticsRoutes from "./routes/analytics.route.js";
-
+import cors from "cors";
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
+
 const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+
+
 const PORT = process.env.PORT || 5000;
 
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" })); // allows you to parse the body of the request
 app.use(cookieParser());
@@ -28,13 +38,13 @@ app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
 
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+// if (process.env.NODE_ENV === "production") {
+// 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
-}
+// 	app.get("*", (req, res) => {
+// 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+// 	});
+// }
 
 app.listen(PORT, () => {
 	console.log("Server is running on http://localhost:" + PORT);
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
+console.log("MODE:", import.meta.env.MODE);
+console.log("Base URL:", import.meta.env.VITE_API_BASE_URL);
 
 const axiosInstance = axios.create({
-	baseURL: import.meta.mode === "development" ? "http://localhost:5000/api" : "/api",
+	// baseURL: import.meta.mode === "development" ? "http://localhost:5000/api" : "/api",
+	baseURL: import.meta.env.VITE_API_BASE_URL,
 	withCredentials: true, // send cookies to the server
 });
 
